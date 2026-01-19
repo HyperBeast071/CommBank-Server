de86b58b82795685b02e16f5f425ba3b0daf46ba
Sagnik Maji <m59078361@gmail.com>
2026-01-19 19:14:47 +0530

    Add optional icon field to Goal model

--- CommBank-Server/Program.cs
+++ CommBank-Server/Program.cs
@@ -11,7 +11,8 @@ builder.Services.AddSwaggerGen();
 
 builder.Configuration.SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("Secrets.json");
 
-var mongoClient = new MongoClient(builder.Configuration.GetConnectionString("CommBank"));
+var mongoConnectionString = builder.Configuration["MongoDbSettings:ConnectionString"];
+var mongoClient = new MongoClient(mongoConnectionString);
 var mongoDatabase = mongoClient.GetDatabase("CommBank");
 
 IAccountsService accountsService = new AccountsService(mongoDatabase);
